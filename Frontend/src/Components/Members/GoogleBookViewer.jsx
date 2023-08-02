aa24bd481b723b44476d4383205cf98296bd1ebe
Sivaprasad <sivaprasadtyl@gmail.com>
2023-08-02 09:44:34 +0530

    Update on google books viewer

--- Frontend/src/Components/Members/GoogleBookViewer.jsx
+++ Frontend/src/Components/Members/GoogleBookViewer.jsx
@@ -1,8 +1,13 @@
 import { useRef } from "react";
 import { useState, useEffect } from "react";
 
-function GoogleBookViewer({ isbn , showViewer , hideReader , showReader , bookReader }) {
-
+function GoogleBookViewer({
+  isbn,
+  showViewer,
+  hideReader,
+  showReader,
+  bookReader,
+}) {
   // Obtain ISBN number of user's current book
   const ISBN_num = isbn;
   const canvasRef = useRef();
@@ -11,54 +16,124 @@ function GoogleBookViewer({ isbn , showViewer , hideReader , showReader , bookRe
   const [loaded, setLoaded] = useState(false);
   // Create alert message if book not found in Google Database
   function alertNotFound() {
-    hideReader()
-    setLoaded(false)
+    alert("could not embed the book!");
   }
-  // Add a Google Books script tag and event listener if the tag has loaded
-  useEffect(() => {
-    const scriptTag = document.createElement("script");
-    scriptTag.src = "https://www.google.com/books/jsapi.js";
-    scriptTag.addEventListener("load", () => {
-      setLoaded(true)
-    });
-    scriptTag.id = "google-script";
-    document.body.appendChild(scriptTag);
-  }, []);
-  // Once Google Books has loaded, then create new instance of Default viewer and load book's information to viewer
-  useEffect(() => {
-    if (!loaded) return;
-    else {
-      if (window.viewer) {
-        let viewer = new window.google.books.DefaultViewer(canvasRef.current);
-        viewer.load("ISBN:" + ISBN_num, alertNotFound); 
-      } else {
-        window.google.books.load();
-        window.google.books.setOnLoadCallback(() => {
-          let viewer = new window.google.books.DefaultViewer(canvasRef.current);
-          window.viewer = viewer;
-          viewer.load("ISBN:" + ISBN_num, alertNotFound);
-        });
-      }
+   // Create alert message if book not found in Google Database
+   function alertNotFound() {            
+       alert("could not embed the book!");
     }
-  }, [loaded , ISBN_num]);
-  return (
-    <div className={`${showViewer ? 'block' : 'hidden'} bg-black/10`}>
-      {loaded ? (
-        <div
-          className="w-[800px] h-[600px]"
-          ref={canvasRef}
-          id="viewerCanvas"
-        ></div>
-      ) : (
-        <div className="w-[800px] h-[600px] bg-black/20">
-          <div className="flex h-full justify-center items-center">
-            <h1 className="text-xl font-nunito font-semibold">Book preview is not available</h1>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
+   // Add a Google Books script tag and event listener if the tag has loaded
+   useEffect(()=> {      
+      const scriptTag = document.createElement('script')                
+      scriptTag.src= 'https://www.google.com/books/jsapi.js'       
+      scriptTag.addEventListener('load', ()=>setLoaded(true))       
+      scriptTag.id = "google-script"      
+      document.body.appendChild(scriptTag);
+      
+      const checkAPITimeout = setTimeout(() => {
+        if (window.google && window.google.books) {
+          console.log('Google Books API loaded successfully!');
+        } else {
+          console.log('Google Books API not available!');
+        }
+      }, 500);
+    
+      return () => {
+        clearTimeout(checkAPITimeout);
+      };
+      
+    }, []);      
+   // Once Google Books has loaded, then create new instance of Default viewer and load book's information to viewer
+   useEffect(()=>{            
+       if (!loaded) return             
+       else{         
+            if(window.viewer){            
+               let viewer = new window.google.books.DefaultViewer
+               (canvasRef.current); 
+               viewer.load('ISBN:'+ ISBN_num, alertNotFound);                    
+             }        
+             else{          
+               window.google.books.load()                             
+               window.google.books.setOnLoadCallback(() => {                 
+               let viewer = new window.google.books.DefaultViewer      
+                   (canvasRef.current);         
+               window.viewer = viewer         
+               viewer.load('ISBN:'+ ISBN_num, alertNotFound);        
+             })
+           }              
+       }}, [loaded])   
+
+      //  useEffect(() => {
+      //   if (window.google && window.google.books) {
+      //     console.log("Google Books API loaded successfully!");
+      //   } else {
+      //     console.log("Google Books API not available!");
+      //   }
+      // }, []);
+      
+       return (      
+          <div>        
+              {loaded ?             
+                   <div>                
+                      <div ref={canvasRef} id="viewerCanvas"></div>            
+                   </div> : 
+              'Script not loaded'}      
+          </div>    )
+;}
+
+// // Initialize loaded state as false
+// const [loaded, setLoaded] = useState(false);
+// // Create alert message if book not found in Google Database
+// function alertNotFound() {
+//   // hideReader()
+//   // setLoaded(false)
+//   alert("book not found")
+// }
+// // Add a Google Books script tag and event listener if the tag has loaded
+// useEffect(() => {
+//   const scriptTag = document.createElement("script");
+//   scriptTag.src = "https://www.google.com/books/jsapi.js";
+//   scriptTag.addEventListener("load", () => {
+//     setLoaded(true)
+//   });
+//   scriptTag.id = "google-script";
+//   document.body.appendChild(scriptTag);
+// }, []);
+// // Once Google Books has loaded, then create new instance of Default viewer and load book's information to viewer
+// useEffect(() => {
+//   if (!loaded) return;
+//   else {
+//     if (window.viewer) {
+//       let viewer = new window.google.books.DefaultViewer(canvasRef.current);
+//       viewer.load("ISBN:" + ISBN_num, alertNotFound);
+//     } else {
+//       window.google.books.load();
+//       window.google.books.setOnLoadCallback(() => {
+//         let viewer = new window.google.books.DefaultViewer(canvasRef.current);
+//         window.viewer = viewer;
+//         viewer.load("ISBN:" + ISBN_num, alertNotFound);
+//       });
+//     }
+//   }
+// }, [loaded , ISBN_num]);
+// return (
+//   <div className={`${showViewer ? 'block' : 'hidden'} bg-black/10`}>
+//     {loaded ? (
+//       <div
+//         className="w-[800px] h-[600px]"
+//         ref={canvasRef}
+//         id="viewerCanvas"
+//       ></div>
+//     ) : (
+//       <div className="w-[800px] h-[600px] bg-black/20">
+//         <div className="flex h-full justify-center items-center">
+//           <h1 className="text-xl font-nunito font-semibold">Book preview is not available</h1>
+//         </div>
+//       </div>
+//     )}
+//   </div>
+// );
+// }
 
 //   useEffect(() => {
 //       const scriptTag = document.createElement("script");
